9b447c1b6803d135b2ea10a6ec745fd2eccc809f
Reimar Rosas <reimarrosas0208@gmail.com>
2022-02-06 16:25:50 -0500

    Edited the home view with the appropriate content (Project Proposal)

--- MVC/app/views/Home/home.php
+++ MVC/app/views/Home/home.php
@@ -1,15 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include_once APPROOT . '/views/includes/header.php' ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sample PHP</title>
-</head>
+<div class="container">
+    <h1 class="mt-5 mb-3">Term Project (LIA) - 420-411-VA: Pokemon Marketplace</h1>
+    <h2 class="mb-4">This project is a PHP ECommerce Web App</h2>
+    <button type="button" class="btn btn-primary mb-4 mx-5">Learn More</button>
 
-<body>
-    <h1>Hello, World!</h1>
-</body>
+    <h3>Project Proposal</h3>
 
-</html>
+    <p>We will build a trading web application where users can list, sell and buy Pokemon Cards.</p>
+
+    <h3 class="mb-3">Our project supports the following features:</h3>
+    <ol class="list-group list-group-numbered">
+        <li class="list-group-item">As a user, I can login and logout into and from my account.</li>
+        <li class="list-group-item">As a user, I can like post from another user</li>
+        <li class="list-group-item">As a user, I can search for cards by their names</li>
+        <li class="list-group-item">As a user, I can add/delete/modify cards in my profile</li>
+        <li class="list-group-item">As a user, I can set the rarity of the cards that I posted from common to legendary</li>
+        <li class="list-group-item">As a user, I can sort post based on their price, likes, rarity, and post date.</li>
+        <li class="list-group-item">As a buyer, I an browse for cards that are available for sale.</li>
+        <li class="list-group-item">As a buyer, I can save posts from another user in case I want to buy them later.</li>
+        <li class="list-group-item">As a buyer, I can have a balance in my account for buying.</li>
+        <li class="list-group-item">As a buyer, I can add/remove cards to a cart for checkout.</li>
+        <li class="list-group-item">As a buyer, I can add stars to a seller's profile to rate their service.</li>
+        <li class="list-group-item">As a buyer, I can offer on a card sold by another user.</li>
+        <li class="list-group-item">As a seller, I can list cards from my profile for sale.</li>
+        <li class="list-group-item">As a seller, I can see the list of offers from other users and select the one I liked the most.</li>
+    </ol>
+
+    <p class="mt-3 mb-5">We will model our solution using Facebook's UI and EBay's functionality. We estimate that we will spend a total of 160 hours building this product, i.e. 80 hours per team member.</p>
+</div>
+
+<?php include_once APPROOT . '/views/includes/footer.php' ?>
