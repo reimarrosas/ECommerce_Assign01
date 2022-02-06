78a73c2b2fd9752df185e46ee8aed567b54576df
Reimar Rosas <reimarrosas0208@gmail.com>
2022-02-06 16:26:21 -0500

    Created a new timeline view that contains the task for each member for the project and their duration

---
+++ MVC/app/views/Home/timeline.php
@@ -0,0 +1,30 @@
+<?php include_once APPROOT . '/views/includes/header.php' ?>
+
+<div class="container my-5">
+  <table class="table">
+    <thead class="thead-dark">
+      <tr>
+        <th class="text-center">Student Name</th>
+        <th class="text-center">Student ID</th>
+        <th class="text-center">Task</th>
+        <th class="text-center">Timeline</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="text-center">Reimar Rosas</td>
+        <td class="text-center">1946923</td>
+        <td class="text-center">Front-End Programming, Back-End Logic</td>
+        <td class="text-center">3 weeks</td>
+      </tr>
+      <tr>
+        <td class="text-center">Krikor Astour</td>
+        <td class="text-center">[PENDING]</td>
+        <td class="text-center">Front-End Programming, Back-End Logic</td>
+        <td class="text-center">3 weeks</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+<?php include_once APPROOT . '/views/includes/footer.php' ?>
