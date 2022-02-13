c7f37aad1c37be7e85970d86222eeb50e72ac7db
Krikor Astour <ararad.seller@gmail.com>
2022-02-13 06:29:51 -0800

    modified fixed about.php

--- MVC/app/views/Home/about.php
+++ MVC/app/views/Home/about.php
@@ -1,214 +1,118 @@
 <?php include_once APPROOT . '/views/includes/header.php' ?>
-<div id="accordion">
-    <div class="card">
-        <div class="card-header" id="headingOne">
-            <h5 class="mb-0">
-                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    Feautre #1
-                </button>
-            </h5>
-        </div>
+<style>
+  .accordion {
+    background-color: #eee;
+    color: #444;
+    cursor: pointer;
+    padding: 18px;
+    width: 100%;
+    border: none;
+    text-align: left;
+    outline: none;
+    font-size: 15px;
+    transition: 0.4s;
+  }
 
-        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-            <div class="card-body">
-                As a user, I can login and logout into and from my account.
-            </div>
-        </div>
-    </div>
-    <div class="card">
-        <div class="card-header" id="headingTwo">
-            <h5 class="mb-0">
-                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                    Feature #2
-                </button>
-            </h5>
-        </div>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-            <div class="card-body">
-                As a user, I can like post from another user
-            </div>
-        </div>
-    </div>
-    <div class="card">
-        <div class="card-header" id="headingThree">
-            <h5 class="mb-0">
-                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                    Feature #3
-                </button>
-            </h5>
-        </div>
-        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-            <div class="card-body">
-                As a user, I can search for cards by their names
+  .active,
+  .accordion:hover {
+    background-color: #ccc;
+  }
 
-            </div>
-        </div>
-    </div>
-</div>
-<div class="card">
-    <div class="card-header" id="headingThree">
-        <h5 class="mb-0">
-            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Feature #4
-            </button>
-        </h5>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-        <div class="card-body">
-            As a user, I can add/delete/modify cards in my profile
+  .panel {
+    padding: 0 18px;
+    display: none;
+    background-color: white;
+    overflow: hidden;
+  }
+</style>
 
-        </div>
-    </div>
+
+
+<h2>About Our Project</h2>
+
+<button class="accordion">Feature 1</button>
+<div class="panel">
+  <p>As a user, I can login and logout into and from my account.</p>
 </div>
+
+<button class="accordion">Feature 2</button>
+<div class="panel">
+  <p>As a user, I can like post from another user</p>
 </div>
-<div class="card">
-    <div class="card-header" id="headingFour">
-        <h5 class="mb-0">
-            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapse" aria-expanded="false" aria-controls="collapse">
-                Feature #5
-            </button>
-        </h5>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-        <div class="card-body">
-            As a user, I can set the rarity of the cards that I posted from common to legendary
-        </div>
-    </div>
+
+<button class="accordion">Feature 3</button>
+<div class="panel">
+  <p>As a user, I can search for cards by their names</p>
 </div>
+<button class="accordion">Feature 4</button>
+<div class="panel">
+  <p>As a user, I can add/delete/modify cards in my profile</p>
 </div>
-<div class="card">
-    <div class="card-header" id="headingThree">
-        <h5 class="mb-0">
-            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseThree">
-                Feature #6
-            </button>
-        </h5>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-        <div class="card-body">
-            As a user, I can sort post based on their price, likes, rarity, and post date.
-        </div>
-    </div>
+
+
+<button class="accordion">Feature 5</button>
+<div class="panel">
+  <p>As a user, I can set the rarity of the cards that I posted from common to legendary</p>
 </div>
+
+<button class="accordion">Feature 6</button>
+<div class="panel">
+  <p>As a user, I can sort post based on their price, likes, rarity, and post date.</p>
 </div>
-<div class="card">
-    <div class="card-header" id="headingThree">
-        <h5 class="mb-0">
-            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Feature #7
-            </button>
-        </h5>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-        <div class="card-body">
-            As a buyer, I an browse for cards that are available for sale.
-        </div>
-    </div>
+
+<button class="accordion">Feature 7</button>
+<div class="panel">
+  <p>As a buyer, I an browse for cards that are available for sale.</p>
 </div>
+
+<button class="accordion">Feature 8</button>
+<div class="panel">
+  <p>As a buyer, I can save posts from another user in case I want to buy them later.</p>
 </div>
-<div class="card">
-    <div class="card-header" id="headingThree">
-        <h5 class="mb-0">
-            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Feature #8
-            </button>
-        </h5>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-        <div class="card-body">
-            As a buyer, I can save posts from another user in case I want to buy them later.
-        </div>
-    </div>
+
+<button class="accordion">Feature 9</button>
+<div class="panel">
+  <p>As a buyer, I can have a balance in my account for buying.</p>
 </div>
+
+<button class="accordion">Feature 10</button>
+<div class="panel">
+  <p>As a buyer, I can add/remove cards to a cart for checkout.</p>
 </div>
-<div class="card">
-    <div class="card-header" id="headingThree">
-        <h5 class="mb-0">
-            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Feature #9
-            </button>
-        </h5>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-        <div class="card-body">
-            As a buyer, I can have a balance in my account for buying.
-        </div>
-    </div>
+
+<button class="accordion">Feature 11</button>
+<div class="panel">
+  <p>As a buyer, I can add stars to a seller's profile to rate their service.</p>
 </div>
+
+<button class="accordion">Feature 12</button>
+<div class="panel">
+  <p>As a buyer, I can offer on a card sold by another user.</p>
 </div>
-<div class="card">
-    <div class="card-header" id="headingThree">
-        <h5 class="mb-0">
-            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Feature #10
-            </button>
-        </h5>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-        <div class="card-body">
-            As a buyer, I can add/remove cards to a cart for checkout.
-        </div>
-    </div>
+
+<button class="accordion">Feature 13</button>
+<div class="panel">
+  <p>As a seller, I can list cards from my profile for sale.</p>
 </div>
+
+<button class="accordion">Feature 14</button>
+<div class="panel">
+  <p>As a seller, I can see the list of offers from other users and select the one I liked the most.</p>
 </div>
-<div class="card">
-    <div class="card-header" id="headingThree">
-        <h5 class="mb-0">
-            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Feature #11
-            </button>
-        </h5>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-        <div class="card-body">
-            As a buyer, I can add stars to a seller's profile to rate their service.
-        </div>
-    </div>
-</div>
-</div>
-<div class="card">
-    <div class="card-header" id="headingThree">
-        <h5 class="mb-0">
-            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Feature #12
-            </button>
-        </h5>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-        <div class="card-body">
-            As a buyer, I can offer on a card sold by another user.
-        </div>
-    </div>
-</div>
-</div>
-<div class="card">
-    <div class="card-header" id="headingThree">
-        <h5 class="mb-0">
-            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Feature #13
-            </button>
-        </h5>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-        <div class="card-body">
-            As a seller, I can list cards from my profile for sale.
-        </div>
-    </div>
-</div>
-</div>
-<div class="card">
-    <div class="card-header" id="headingThree">
-        <h5 class="mb-0">
-            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Feature #14
-            </button>
-        </h5>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-        <div class="card-body">
-            As a seller, I can see the list of offers from other users and select the one I liked the most.
-        </div>
-    </div>
-</div>
-</div>
+<script>
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
+</script>
 <?php include_once APPROOT . '/views/includes/footer.php' ?>
